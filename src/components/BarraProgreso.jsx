b24788267746d8b27277e4089bb18a4c32f6cd84
Jaime A <jaimeangel1097@gmail.com>
2023-10-08 23:17:53 -0500

    solucionando bug barra de progreso

--- src/components/BarraProgreso.jsx
+++ src/components/BarraProgreso.jsx
@@ -50,7 +50,14 @@ function BarraProgreso() {
     return (
         <div className="w-full  bg-white rounded-lg px-10 py-6 flex flex-col">
             <div className="w-full border rounded-2xl shadow-inner">
-                <div className={`w-[${porcentajeActual}%] h-3 rounded-2xl bg-green-500`}></div>
+                <div
+                    style={{
+                        height:'12px',
+                        width:`${porcentajeActual}%`,
+                        borderRadius: '1rem',
+                        backgroundColor: 'rgb(34 197 94)'
+                    }} 
+                ></div>
             </div>
             <div className="w-full flex flex-row justify-between mt-5 items-center">
                 {
