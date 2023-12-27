56628985c467184a14d38ace0b24f0f97790f050
Jaime A <jaimeangel1097@gmail.com>
2023-12-26 22:01:50 -0500

    refactorizando componente condiciones comerciales

--- src/components/crear cotizaciones/CondicionesComerciales.jsx
+++ src/components/crear cotizaciones/CondicionesComerciales.jsx
@@ -18,7 +18,7 @@ function CondicionesComerciales({
     numeroPasos,
     pasoActual
 }){
-    //condiciones comerciales
+    // condiciones comerciales
     const [condiciones,setCondiciones]=useState([
         {
             id:1,
@@ -40,22 +40,20 @@ function CondicionesComerciales({
         }
     ])
 
-    //agregar condicion
-    const [agregar,setAgregar]=useState(false)
-    //datos nueva condicion
+    // nueva condicion comercial
+    const [agregarCondicion,setAgregarCondicion]=useState(false)
     const [tipo,setTipo]=useState('')
     const [text,setText]=useState('')
-    //notas comerciales
+
+    // notas comerciales
     const [notas,setNotas]=useState('')
 
-    //handle cambiar de paso
     const validateCambiarPaso=()=>{
         if(pasoActual===numeroPasos) return
         cambiarPaso(value=>value+1)
     }
     
-    //cambiar estado de eleccion de las condiciones
-    const cambiarEstado=(id)=>{
+    const cambiarEstadoEleccionCondiciones=(id)=>{
         const categoriesModificado = condiciones.map(item => {
             if(item.id === id) {
                 item.selected=!item.selected
@@ -68,12 +66,7 @@ function CondicionesComerciales({
         setCondiciones(categoriesModificado)
     }
 
-    //mostrar ocultar formulario para agregar nueva condicion
-    const showAgregarCondicion=()=>{
-        setAgregar(true)
-    }
-
-    const agregarCondicion=()=>{
+    const agregarCondicionNueva=()=>{
         const newCondicion={
             id:generarIdNumerico(),
             titulo:tipo,
@@ -84,22 +77,16 @@ function CondicionesComerciales({
 
         setTipo('')
         setText('')
-        setAgregar(false)
-        /* cambiarEstado(newCondicion.id) */
+        setAgregarCondicion(false)
     }
 
-    //verificar si exite objeto
-    function verificarExistenciaID(array, idBuscado) {
-        return array.some(objeto => objeto.id === idBuscado);
-    }
-    //cancelar agregar condicion y limpiar el estado
     const cancelarAgregarCondicion=()=>{
-        setAgregar(false)
         setTipo('')
         setText('')
+        setAgregarCondicion(false)
     }
 
-    //validacion para cambiar paso
+    // validacion para cambiar paso
     useEffect(()=>{
         if(validatePaso){
             setValidatePaso(false)
@@ -107,34 +94,35 @@ function CondicionesComerciales({
         }
     },[validatePaso])
 
-    //persistencia de informacion
+    // persistencia de informacion
     useEffect(()=>{
         if('condiciones' in cotizacion){
-            if(Object.keys(cotizacion.condiciones).length !== 0){
-                const existe = verificarExistenciaID(condiciones, cotizacion.condiciones.id);
-                if(existe){
-                    const condicionesModificado = condiciones.map((item) => {
-                        if(item.id === cotizacion.condiciones.id){
-                            return cotizacion.condiciones;
-                        }else{
-                            return item;
-                        }
-                    });
-                    setCondiciones(condicionesModificado)
-                }else{
-                    setCondiciones([cotizacion.condiciones,...condiciones])
-                }
+            // verifica si la condicion guardada pertenece a las predeterminadas
+            const existe = condiciones.some(objeto => objeto.id === cotizacion.condiciones.id);
+            
+            if(existe){
+                const condicionesModificado = condiciones.map((item) => {
+                    if(item.id === cotizacion.condiciones.id){
+                        return cotizacion.condiciones;
+                    }else{
+                        return item;
+                    }
+                });
+                setCondiciones(condicionesModificado)
+            }
+
+            // si no pertenece es porque fue una condicion personalizada creada
+            if(!existe && Object.keys(cotizacion.condiciones).length !==0){
+                setCondiciones([cotizacion.condiciones,...condiciones])
             }
         }
 
         if('notas' in cotizacion){
-            if(cotizacion.notas !== ''){
-                setNotas(cotizacion.notas)
-            }
+            setNotas(cotizacion.notas)
         }
     },[])
     
-    //actulizando informacion en state principal [condiciones]
+    //actulizando condiciones state principal
     useEffect(()=>{
         const condicionesModificado = condiciones.find((item) => {
             if(item.selected === true){
@@ -151,15 +139,13 @@ function CondicionesComerciales({
         }
     },[condiciones])
 
-    //actulizando informacion en state principal [notas]
+    //actulizando notas en state princiapal
     useEffect(()=>{
-        if(notas.length !==''){
-            const newData={
-                ...cotizacion,
-                notas:notas
-            }
-            setCotizacion(newData)
+        const newData={
+            ...cotizacion,
+            notas:notas
         }
+        setCotizacion(newData)
     },[notas])
 
 
@@ -188,7 +174,7 @@ function CondicionesComerciales({
                                 <div className='flex flex-col'>
                                     <h1 className="text-lg font-semibold italic text-justify px-5">Selecciona una opción de terminos de pago predefinida o crea tu propio término de pago personalizado</h1>
                                     {
-                                        agregar && (
+                                        agregarCondicion ? 
                                             <div className='flex flex-col gap-3 my-2 shadow px-5 py-3 rounded-lg border'>
                                                 <div className='flex flex-row gap-3'>
                                                     <p className='font-bold mb-1'>Tipo:</p>
@@ -207,7 +193,7 @@ function CondicionesComerciales({
                                                 ></textarea>
                                                 <div className='flex flex-row gap-5'>
                                                     <button
-                                                        onClick={agregarCondicion}
+                                                        onClick={agregarCondicionNueva}
                                                         className="w-[7rem] first-letter:uppercase rounded  bg-yellow-300 border-2 border-yellow-500 font-semibold tracking-wide"
                                                     >
                                                         agregar
@@ -220,17 +206,13 @@ function CondicionesComerciales({
                                                     </button>
                                                 </div>
                                             </div>                                
-                                        )
-                                    }
-                                    {
-                                        !agregar && (
+                                        :
                                             <button
-                                                onClick={showAgregarCondicion}
+                                                onClick={()=>setAgregarCondicion(true)}
                                                 className="self-end w-[20rem] my-1 first-letter:uppercase rounded  bg-yellow-300 border-2 border-yellow-500 font-semibold tracking-wide"
                                             >
                                                 crear termino de pago personalizado
                                             </button>
-                                        )
                                     }
                                     {
                                         condiciones.map((condicion)=>(
@@ -240,7 +222,7 @@ function CondicionesComerciales({
                                                 <div className='text-end mt-2'>
                                                     <SwitchButtonPequeño
                                                         enabled={condicion.selected}
-                                                        setEnabled={()=>cambiarEstado(condicion.id)}
+                                                        setEnabled={()=>cambiarEstadoEleccionCondiciones(condicion.id)}
                                                     />
                                                 </div>
                                             </div>
--- src/pages/cotizaciones/CrearCotizacion.jsx
+++ src/pages/cotizaciones/CrearCotizacion.jsx
@@ -16,7 +16,7 @@ import pasos from "../../data/pasosCotizacion.js";
 function CrearCotizacion() {
   //botones cambiar paso
   const numeroPasos=pasos.length;
-  const [pasoActual,setPasoActual]=useState(3)
+  const [pasoActual,setPasoActual]=useState(4)
   const [validatePaso,setValidatePaso]=useState(false)
 
   //datos cotizacion
