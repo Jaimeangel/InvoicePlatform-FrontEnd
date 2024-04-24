27ad5921e917324e253e82ad014afab66bb25fff
Jaime A <jaimeangel1097@gmail.com>
2024-04-24 15:47:41 -0500

    creando wraper de alerta de errores

---
+++ src/components/alertas/AlertaWrapper.jsx
@@ -0,0 +1,45 @@
+//font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { faFlag } from "@fortawesome/free-solid-svg-icons";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+
+function AlertaWrapper({
+    children,
+    alert,
+    close=false
+}) {
+        
+    const {
+        msg,
+        error
+    }=alert;
+
+    return (
+        <div className="flex flex-col">
+            {
+                error &&
+                <div className={`${error ?'bg-red-100 border border-red-600':'bg-blue-100 border border-blue-600'} w-full flex flex-row justify-between rounded-md mt-3 border-2`}>
+                    <div className={`${error ?'bg-red-600':'bg-blue-600'} px-4`}>
+                        {
+                            error ? <FontAwesomeIcon icon={faTriangleExclamation} style={{color: "#ffffff",}} /> : <FontAwesomeIcon icon={faFlag} style={{color: "#ffffff",}} />
+                        }
+                    </div>
+                    <div className="w-full my-auto">
+                        <p className=" tracking-wider text-black text-center text-base font-semibold italic">{msg}</p>
+                    </div>
+                    { close && (
+                        <div className={`${error ?'bg-red-600':'bg-blue-500'} px-4`}>
+                            <FontAwesomeIcon icon={faCircleXmark} style={{color: "#ffffff",}} />
+                        </div>
+                    )
+
+                    }
+                </div>
+            }
+            {children}
+        </div>
+    )
+}
+
+export default AlertaWrapper
--- src/pages/cotizaciones/VisualizarCotizaciones.jsx
+++ src/pages/cotizaciones/VisualizarCotizaciones.jsx
@@ -2,32 +2,46 @@ import { useEffect, useState } from 'react'
 
 import CardCotizaciones from '../../components/ver cotizaciones/CardCotizaciones'
 import EncabezadoCotizaciones from '../../components/ver cotizaciones/EncabezadoCotizaciones'
-
-import useCotizacion from '../../hooks/useCotizacion'
-
 import FiltroPaginacion from '../../components/filtroPaginacion/FiltroPaginacion'
+import AlertaWrapper from '../../components/alertas/AlertaWrapper'
 
 import { FormatoVisualCotizacion } from '../../helpers/FormatoVisualCotizacion'
+import useCotizacion from '../../hooks/useCotizacion'
 
 function VisualizarCotizaciones() {
 
     const {
         obtenerCotizaciones
     }=useCotizacion()
+
+    const [catchError,setCatchError]=useState({
+        msg:false,
+        error:''
+    })
     
     const [cotizaciones,setCotizaciones]=useState([])
     const [cotizacionesFormateadas,setCotizacionesFormateadas]=useState([])
 
+    const obtenerCotizacionesUser = async ()=>{
+        try{
+            const cotizaciones =  await obtenerCotizaciones()
+            setCotizaciones(cotizaciones)
+        } catch (err) {
+            setCatchError({
+                error:true,
+                msg:err.message
+            })
+
+            setTimeout(()=>{
+                setCatchError({
+                    error:false,
+                    msg:''
+                })
+            },15000)
+        }
+    }
     
     useEffect(()=>{
-        const obtenerCotizacionesUser = async ()=>{
-            try{
-                const cotizaciones =  await obtenerCotizaciones()
-                setCotizaciones(cotizaciones)
-            } catch (error) {
-                console.log(error)
-            }
-        }
         obtenerCotizacionesUser()
     },[])
 
@@ -39,13 +53,15 @@ function VisualizarCotizaciones() {
 
     return (
         <div className="w-full bg-white rounded-lg px-10 py-6 shadow-md">
-            <h1 className="mt-2 mb-5 text-3xl font-bold">Cotizaciones</h1>
-            <FiltroPaginacion
-                WraperEncabezado={EncabezadoCotizaciones}
-                CardItems={CardCotizaciones}
-                lista={cotizacionesFormateadas}
-                itemsPaginacion={4}
-            />
+            <AlertaWrapper alert={catchError}>
+                <h1 className="mt-2 mb-5 text-3xl font-bold">Cotizaciones</h1>
+                <FiltroPaginacion
+                    WraperEncabezado={EncabezadoCotizaciones}
+                    CardItems={CardCotizaciones}
+                    lista={cotizacionesFormateadas}
+                    itemsPaginacion={4}
+                />
+            </AlertaWrapper>
         </div>
     )
 }
